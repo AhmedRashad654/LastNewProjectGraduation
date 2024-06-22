3026488861d18eec54b7be6a529b8a4bddba2a9b
AhmedRashad654 <ahmedrashad6542@gmail.com>
2024-06-22 22:22:11 +0300

    lastNow

--- src/Componenets/Layout/Layout.jsx
+++ src/Componenets/Layout/Layout.jsx
@@ -2,26 +2,23 @@ import React, { useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
-import { useQuery } from "react-query";
-import { request } from "../../axios/axios";
 import { setWishList } from "../../slice/slice";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
+import { getAllWishList } from "../../api/api";
 
 export default function Layout() {
-    const user = JSON.parse(localStorage.getItem("user"));
-    const dispatch = useDispatch();
-  ///////////get All WishList//////////
-  function getAllWishList() {
-    return request.get(`/wishList/${user?._id}`);
-  }
-  const { data } = useQuery("getAllWishLish", getAllWishList);
-  useEffect(() => {
-    if (data) {
-      dispatch(setWishList(data?.data?.data?.products));
-    }
-  }, [ data, dispatch ] );
+  const dispatch = useDispatch();
 
-  
+  useEffect(() => {
+    async function getAllList() {
+      const result = await getAllWishList();
+      dispatch(setWishList(result?.data?.data?.products));
+    }
+    if (localStorage.getItem("token")) {
+      getAllList();
+    }
+  }, [dispatch]);
+
   return (
     <>
       <Navbar />
--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,8 +1,13 @@
 import { request } from "../axios/axios";
-
+const user = JSON.parse(localStorage.getItem("user"));
 //////////getAllCategory//////////
 export function getAllCategory() {
   return request.get("/categories");
+}
+///////////////////
+export async function getAllWishList() {
+  const result =await request.get( `/wishList/${user?._id}` );
+  return result
 }
 ////////add from list////////////
 export async function handleAddWishLish(id) {
@@ -24,7 +29,7 @@ export async function handleAddWishLish(id) {
   }
 }
 ///////remove from list////////////
-const user = JSON.parse(localStorage.getItem("user"));
+
 export async function removeFromWishList(id) {
   try {
     const result = await request.patch(`/wishList/${user?._id}/${id}`, null, {
